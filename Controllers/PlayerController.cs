923478a9efccfb279419e2e84e14fcb6f7a9b6b9
Will Maynard <will@willmaynard.com>
2022-03-29 11:24:48 -0700

    Fix for player-service 403s

--- Controllers/PlayerController.cs
+++ Controllers/PlayerController.cs
@@ -7,14 +7,27 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using Rumble.Platform.Common.Services;
 using Rumble.Platform.Common.Utilities;
+using Rumble.Platform.Common.Web;
 
 namespace tower_admin_portal.Controllers
 {
     [Authorize]
     public class PlayerController : Controller
     {
+// #pragma warning disable CS0169
+        private ApiService _apiService;
+        private DynamicConfigService _dynamicConfigService;
+// #pragma warning restore CS0169
+        
         public readonly HttpClient client = new HttpClient(); // should be used for all
+
+        public PlayerController(ApiService api, DynamicConfigService dynamicConfig)
+        {
+            _apiService = api;
+            _dynamicConfigService = dynamicConfig; // Dynamic config service may be changing soon, or rewritten as part of portal, TBD.
+        }
         
         public async Task<IActionResult> Search(string query)
         {
@@ -26,25 +39,29 @@ namespace tower_admin_portal.Controllers
                 List<string> searchUser = new List<string>();
 
                 string requestUrl = "https://dev.nonprod.tower.cdrentertainment.com/player/v2/admin/search?term=" + query;
-
-                object response = null;
                 
-                string token = PlatformEnvironment.Require("PLAYER_TOKEN");
+                string token = _dynamicConfigService.GameConfig.Require<string>("playerServiceToken");
+
+                // Use the API Service to simplify web requests
+                _apiService
+                    .Request($"https://dev.nonprod.tower.cdrentertainment.com/player/v2/admin/search?term={query}")
+                    .AddAuthorization(token)
+                    .OnSuccess(((sender, apiResponse) =>
+                    {
+                        Log.Local(Owner.Nathan, "Request to player-service-v2 succeeded.");
+                    }))
+                    .OnFailure(((sender, apiResponse) =>
+                    {
+                        Log.Error(Owner.Nathan, "Request to player-service-v2 failed.", data: new
+                        {
+                            Response = apiResponse
+                        });
+                    }))
+                    .Get(out GenericData response, out int code);
+                
+                // string token = PlatformEnvironment.Require("PLAYER_TOKEN");
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-                
-                HttpResponseMessage httpResponse = await client.GetAsync(requestUrl);
 
-                if (httpResponse.StatusCode == HttpStatusCode.OK)
-                {
-                    string responseBody = await httpResponse.Content.ReadAsStringAsync();
-                    response = JsonConvert.DeserializeObject<object>(responseBody);
-                }
-                else
-                {
-                    // Log.Error(owner: Owner.Nathan, message: "Request to player service v2 failed.", data: $"Response code {httpResponse.StatusCode}");
-                    response = httpResponse.StatusCode;
-                }
-                
                 searchId.Add("id");
                 searchId.Add("id2");
                 searchUser.Add("user");
@@ -97,6 +114,11 @@ namespace tower_admin_portal.Controllers
             ViewData["Response"] = response;
 
             return View();
+        }
+
+        public ActionResult HealthCheck()
+        {
+            return Ok();
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -6,11 +6,16 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Rumble.Platform.Common.Services;
 using Rumble.Platform.Common.Utilities;
 using tower_admin_portal.Utilities;
 
 namespace tower_admin_portal
 {
+    // This should use PlatformStartup, and if changes are necessary to this to get the site up and running, common should be updated.
+    // Without inheriting from PlatformStartup, many features of platform-common won't work out of the box; for example, the dependency injection
+    // of common services.
+    // TODO: Will to convert this
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -23,6 +28,7 @@ namespace tower_admin_portal
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // base.ConfigureServices(services, Owner.Nathan, warnMS: 30_000, errorMS: 60_000, criticalMS: 90_000, webServerEnabled: true);
             // services.ConfigureApplicationCookie(options => options.LoginPath = "/account/google-login");
             
             services.AddAuthentication(options =>
@@ -48,6 +54,9 @@ namespace tower_admin_portal
                     policy => policy.RequireClaim(ClaimTypes.Email)
                         .AddRequirements(new DomainRequirement("rumbleentertainment.com")));
             });
+
+            services.AddSingleton<ApiService>();
+            services.AddSingleton<DynamicConfigService>();
             
             services.AddControllersWithViews();
         }
