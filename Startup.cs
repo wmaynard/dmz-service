fe2031310c87cab13ee1d5aa885de3e25dce480e
Will Maynard <will@willmaynard.com>
2022-04-04 16:33:33 -0700

    More cookie options; trying to convince Chrome to behave

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using MongoDB.Bson.Serialization;
 using Rumble.Platform.Common.Attributes;
 using Rumble.Platform.Common.Extensions;
@@ -37,6 +38,25 @@ public class Startup : PlatformStartup
             : "";
         
         services.ConfigureApplicationCookie(options => options.LoginPath = $"{baseRoute}/account/google-login");
+
+        services.Configure<CookiePolicyOptions>(options =>
+        {
+            options.MinimumSameSitePolicy = SameSiteMode.Unspecified;
+            options.OnAppendCookie = (cookieContext) =>
+            {
+                CookieOptions cookieOptions = cookieContext.CookieOptions;
+                HttpContext context = cookieContext.Context;
+                
+                if (cookieOptions.SameSite == SameSiteMode.None) 
+                { 
+                    var userAgent = context.Request.Headers["User-Agent"].ToString(); 
+                    if ( DisallowsSameSiteNone(userAgent)) 
+                    { 
+                        cookieOptions.SameSite = SameSiteMode.Unspecified; 
+                    } 
+                } 
+            };
+        });
         
         services.AddAuthentication(configureOptions: options =>
             {
@@ -104,5 +124,42 @@ public class Startup : PlatformStartup
         });
 
         services.AddControllersWithViews();
+    }
+    
+    // More debugging on cookie conflicts
+    // https://community.auth0.com/t/correlation-failed-unknown-location-error-on-chrome-but-not-in-safari/40013/7
+    public bool DisallowsSameSiteNone(string userAgent)
+    {
+        // Check if a null or empty string has been passed in, since this
+        // will cause further interrogation of the useragent to fail.
+        if (String.IsNullOrWhiteSpace(userAgent))
+            return false;
+
+        // Cover all iOS based browsers here. This includes:
+        // - Safari on iOS 12 for iPhone, iPod Touch, iPad
+        // - WkWebview on iOS 12 for iPhone, iPod Touch, iPad
+        // - Chrome on iOS 12 for iPhone, iPod Touch, iPad
+        // All of which are broken by SameSite=None, because they use the iOS networking
+        // stack.
+        if (userAgent.Contains("CPU iPhone OS 12") || userAgent.Contains("iPad; CPU OS 12"))
+            return true;
+
+        // Cover Mac OS X based browsers that use the Mac OS networking stack. 
+        // This includes:
+        // - Safari on Mac OS X.
+        // This does not include:
+        // - Chrome on Mac OS X
+        // Because they do not use the Mac OS networking stack.
+        if (userAgent.Contains("Macintosh; Intel Mac OS X 10_14") && userAgent.Contains("Version/") && userAgent.Contains("Safari"))
+                return true;
+
+        // Cover Chrome 50-69, because some versions are broken by SameSite=None, 
+        // and none in this range require it.
+        // Note: this covers some pre-Chromium Edge versions, 
+        // but pre-Chromium Edge does not require SameSite=None.
+        if (userAgent.Contains("Chrome/5") || userAgent.Contains("Chrome/6"))
+            return true;
+
+        return false;
     }
 }
