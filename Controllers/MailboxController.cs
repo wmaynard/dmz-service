33351266dbdc9c4fc8e3ffedba75bf388e96a588
Nathan Mac <nathan.mac@rumbleentertainment.com>
2022-04-19 17:56:49 -0700

    accidentally forgot some controller changes

--- Controllers/MailboxController.cs
+++ Controllers/MailboxController.cs
@@ -92,7 +92,7 @@ public class MailboxController : PlatformController
                 status: Message.StatusType.UNCLAIMED, internalNote: internalNote, forAccountsBefore: forAccountsBeforeUnix);
 
             _apiService
-                .Request($"https://dev.nonprod.tower.cdrentertainment.com/mail/admin/global/messages/send")
+                .Request(requestUrl + "/send")
                 //.Request($"https://localhost:5071/mail/admin/messages/send") // local
                 .AddAuthorization(token)
                 .SetPayload(new GenericData
@@ -190,7 +190,7 @@ public class MailboxController : PlatformController
                 status: Message.StatusType.UNCLAIMED, internalNote: internalNote);
 
             _apiService
-                .Request($"https://dev.nonprod.tower.cdrentertainment.com/mail/admin/messages/send")
+                .Request(requestUrl)
                 //.Request($"https://localhost:5071/mail/admin/messages/send") // local
                 .AddAuthorization(token)
                 .SetPayload(new GenericData
@@ -223,15 +223,101 @@ public class MailboxController : PlatformController
     }
 
     [Route("edit")]
-    public async void Edit()
+    public async void Edit(string messageId, string subject, string body, string attachments, string visibleFrom, string expiration,
+        string icon, string banner, string status, string internalNote, string forAccountsBefore)
     {
+        string token = _dynamicConfigService.GameConfig.Require<string>("mailToken");
+        string requestUrl = $"{PlatformEnvironment.Optional<string>("PLATFORM_URL").TrimEnd('/')}/mail/admin/global/messages/edit";
         
+        ViewData["Exist"] = "";
+        
+        try
+        {
+            List<Attachment> attachmentsList = ParseMessageData.ParseAttachments(attachments);
+            icon = ParseMessageData.ParseEmpty(icon);
+            banner = ParseMessageData.ParseEmpty(banner);
+            long expirationUnix = ParseMessageData.ParseDateTime(expiration);
+            long visibleFromUnix = ParseMessageData.ParseDateTime(visibleFrom);
+            long? forAccountsBeforeUnix = ParseMessageData.ParseDateTime(forAccountsBefore);
+            
+            _apiService
+                .Request(requestUrl)
+                //.Request($"https://localhost:5071/mail/admin/messages/edit") // local
+                .AddAuthorization(token)
+                .SetPayload(new GenericData
+                {
+                    {"messageId", messageId},
+                    {"subject", subject},
+                    {"body", body},
+                    {"attachments", attachmentsList},
+                    {"expiration", expirationUnix},
+                    {"visibleFrom", visibleFromUnix},
+                    {"icon", icon},
+                    {"banner", banner},
+                    {"status", status},
+                    {"internalNote", internalNote},
+                    {"forAccountsBefore", forAccountsBeforeUnix}
+                })
+                .OnSuccess(((sender, apiResponse) =>
+                {
+                    ViewData["Success"] = "Successfully edited message.";
+                    Log.Local(Owner.Nathan, "Request to mailbox-service succeeded.");
+                }))
+                .OnFailure(((sender, apiResponse) =>
+                {
+                    ViewData["Success"] = "Failed to edit message.";
+                    Log.Error(Owner.Nathan, "Request to mailbox-service failed.", data: new
+                    {
+                        Response = apiResponse
+                    });
+                }))
+                .Patch(out GenericData editResponse, out int editCode);
+        }
+        catch (Exception e)
+        {
+            ViewData["Success"] = "Failed to edit message. Some fields may be malformed.";
+            Log.Error(owner: Owner.Nathan, message: "Error occurred when editing global message.", data: e.Message);
+        }
     }
     
     [Route("delete")]
     public async void Delete(string id)
     {
+        string token = _dynamicConfigService.GameConfig.Require<string>("mailToken");
+        string requestUrl = $"{PlatformEnvironment.Optional<string>("PLATFORM_URL").TrimEnd('/')}/mail/admin/global/messages/delete";
         
+        ViewData["Exist"] = "";
+        
+        try
+        {
+            _apiService
+                .Request(requestUrl)
+                //.Request($"https://localhost:5071/mail/admin/messages/delete") // local
+                .AddAuthorization(token)
+                .SetPayload(new GenericData
+                {
+                    {"messageId", id}
+                })
+                .OnSuccess(((sender, apiResponse) =>
+                {
+                    ViewData["Success"] = "Successfully deleted message.";
+                    Log.Local(Owner.Nathan, "Request to mailbox-service succeeded.");
+                }))
+                .OnFailure(((sender, apiResponse) =>
+                {
+                    ViewData["Success"] = "Failed to delete message.";
+                    Log.Error(Owner.Nathan, "Request to mailbox-service failed.", data: new
+                    {
+                        Response = apiResponse
+                    });
+                }))
+                .Patch(out GenericData deleteResponse, out int deleteCode);
+        }
+        catch (Exception e)
+        {
+            ViewData["Success"] = "Failed to delete message.";
+            Log.Error(owner: Owner.Nathan, message: "Error occurred when deleting global message.", data: e.Message);
+        }
     }
 
     [Route("health")]
