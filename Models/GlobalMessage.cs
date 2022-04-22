82fc0777d0529504e5ad50c5a475f2dc42550d59
Nathan Mac <nathan.mac@rumbleentertainment.com>
2022-04-22 15:28:53 -0700

    formatting in models

--- Models/GlobalMessage.cs
+++ Models/GlobalMessage.cs
@@ -7,19 +7,19 @@ namespace TowerPortal.Models;
 [BsonIgnoreExtraElements]
 public class GlobalMessage : Message
 {
-internal const string DB_KEY_FOR_ACCOUNTS_BEFORE = "acctsbefore";
+    internal const string DB_KEY_FOR_ACCOUNTS_BEFORE = "acctsbefore";
 
-        public const string FRIENDLY_KEY_FOR_ACCOUNTS_BEFORE = "forAccountsBefore";
-        
-        [BsonElement(DB_KEY_FOR_ACCOUNTS_BEFORE)]
-        [JsonInclude, JsonPropertyName(FRIENDLY_KEY_FOR_ACCOUNTS_BEFORE)]
-        public long? ForAccountsBefore { get; private set; }
+    public const string FRIENDLY_KEY_FOR_ACCOUNTS_BEFORE = "forAccountsBefore";
+    
+    [BsonElement(DB_KEY_FOR_ACCOUNTS_BEFORE)]
+    [JsonInclude, JsonPropertyName(FRIENDLY_KEY_FOR_ACCOUNTS_BEFORE)]
+    public long? ForAccountsBefore { get; private set; }
 
-        public GlobalMessage(string subject, string body, List<Attachment> attachments, long expiration,
-            long visibleFrom, string icon, string banner, StatusType status, string internalNote, long? forAccountsBefore = null)
-            : base(subject: subject, body: body, attachments: attachments, expiration: expiration,
-                visibleFrom: visibleFrom, icon: icon, banner: banner, status: status, internalNote: internalNote)
-        {
-            ForAccountsBefore = forAccountsBefore;
-        }
+    public GlobalMessage(string subject, string body, List<Attachment> attachments, long expiration,
+        long visibleFrom, string icon, string banner, StatusType status, string internalNote, long? forAccountsBefore = null)
+        : base(subject: subject, body: body, attachments: attachments, expiration: expiration,
+            visibleFrom: visibleFrom, icon: icon, banner: banner, status: status, internalNote: internalNote)
+    {
+        ForAccountsBefore = forAccountsBefore;
+    }
 }
