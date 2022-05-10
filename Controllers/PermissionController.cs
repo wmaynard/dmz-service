a36fe284fe16bc25cb35f692ae8e2c83d2f1836d
Nathan Mac <nathan.mac@rumbleentertainment.com>
2022-05-09 18:43:16 -0700

    completed base role management functionality

--- Controllers/PermissionController.cs
+++ Controllers/PermissionController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
@@ -57,7 +58,7 @@ public class PermissionController : PlatformController
     }
 
     [Route("account")]
-    public async Task<IActionResult> Account(string accountId)
+    public async Task<IActionResult> Account(string id)
     {
         // Checking access permissions
         Account account = Models.Account.FromGoogleClaims(User.Claims); // Models required for some reason?
@@ -74,18 +75,33 @@ public class PermissionController : PlatformController
         {
             return View("Error");
         }
+
+        List<String> environments = new List<String>();
+        environments.Add("107"); // currently hard coded 107
+        environments.Add("207"); // currently hard coded 207
+        environments.Add("217"); // currently hard coded 217
+        environments.Add("227"); // currently hard coded 227
+        ViewData["Envs"] = environments;
         
-        Account user = _accountService.Get(accountId);
+        Account user = _accountService.Get(id);
         
-        ViewData["Account"] = user;
+        TempData["AccountId"] = id;
+        ViewData["Account"] = user.Email;
 
         return View();
     }
 
     [HttpPost]
     [Route("account")]
-    public async Task<IActionResult> Account(string accountId, List<string> roles)
+    public async Task<IActionResult> Account(string id,
+        string viewPlayer107, string editPlayer107, string viewMailbox107, string editMailbox107,
+        string viewPlayer207, string editPlayer207, string viewMailbox207, string editMailbox207,
+        string viewPlayer217, string editPlayer217, string viewMailbox217, string editMailbox217,
+        string viewPlayer227, string editPlayer227, string viewMailbox227, string editMailbox227
+        )
     {
+        // TODO show current roles, confirmation on save
+        
         // Checking access permissions
         Account account = Models.Account.FromGoogleClaims(User.Claims); // Models required for some reason?
         string admin = _accountService.CheckPermissions(account, "admin");
@@ -101,14 +117,88 @@ public class PermissionController : PlatformController
         {
             return View("Error");
         }
+
+        Account user = _accountService.Get(id);
+
+        List<string> roles = new List<string>();
         
-        Account user = _accountService.Get(accountId);
+        if (user.Roles.Contains("admin")) // prevent remove own admin
+        {
+            roles.Add("admin");
+        }
         
+        // Need to find a way to not have to manually put in each separate permission in parameters
+        // Seems like name field in views has to be hardcoded
+        if (viewPlayer107 != null)
+        {
+            roles.Add("viewPlayer107");
+        }
+        if (viewPlayer207 != null)
+        {
+            roles.Add("viewPlayer207");
+        }
+        if (viewPlayer217 != null)
+        {
+            roles.Add("viewPlayer217");
+        }
+        if (viewPlayer227 != null)
+        {
+            roles.Add("viewPlayer227");
+        }
+        if (editPlayer107 != null)
+        {
+            roles.Add("editPlayer107");
+        }
+        if (editPlayer207 != null)
+        {
+            roles.Add("editPlayer207");
+        }
+        if (editPlayer217 != null)
+        {
+            roles.Add("editPlayer217");
+        }
+        if (editPlayer227 != null)
+        {
+            roles.Add("editPlayer227");
+        }
+        if (viewMailbox107 != null)
+        {
+            roles.Add("viewMailbox107");
+        }
+        if (viewMailbox207 != null)
+        {
+            roles.Add("viewMailbox207");
+        }
+        if (viewMailbox217 != null)
+        {
+            roles.Add("viewMailbox217");
+        }
+        if (viewMailbox227 != null)
+        {
+            roles.Add("viewMailbox227");
+        }
+        if (editMailbox107 != null)
+        {
+            roles.Add("editMailbox107");
+        }
+        if (editMailbox207 != null)
+        {
+            roles.Add("editMailbox207");
+        }
+        if (editMailbox217 != null)
+        {
+            roles.Add("editMailbox217");
+        }
+        if (editMailbox227 != null)
+        {
+            roles.Add("editMailbox227");
+        }
+
         _accountService.UpdateRoles(user, roles);
         
-        ViewData["Account"] = user;
+        ViewData["Account"] = user.Email;
         
-        return View();
+        return RedirectToAction("Account", new { id = id });
     }
     
     [Route("health")]
