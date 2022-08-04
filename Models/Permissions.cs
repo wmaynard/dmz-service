d83239389c80cf1caf7614243500a33d585f2587
Nathan Mac <nathan.mac@rumbleentertainment.com>
2022-08-03 17:09:51 -0700

    platform upgrade

--- Models/Permissions.cs
+++ Models/Permissions.cs
@@ -7,31 +7,31 @@ namespace TowerPortal.Models;
 [BsonIgnoreExtraElements]
 public class Permissions : PlatformDataModel
 {
-	// TODO: Permissions should be renamed to be more English-readable
-	// e.g. IsAdmin
-	// This will make it easier to understand when used in conditional statements
-	public bool Admin { get; set; }
-	public bool ManagePermissions { get; set; }
-	public bool ViewPlayer { get; set; }
-	public bool EditPlayer { get; set; }
-	public bool ViewMailbox { get; set; }
-	public bool EditMailbox { get; set; }
-	public bool ViewToken { get; set; }
-	public bool EditToken { get; set; }
-	public bool ViewConfig { get; set; }
-	public bool EditConfig { get; set; }
+  // TODO: Permissions should be renamed to be more English-readable
+  // e.g. IsAdmin
+  // This will make it easier to understand when used in conditional statements
+  public bool Admin { get; set; }
+  public bool ManagePermissions { get; set; }
+  public bool ViewPlayer { get; set; }
+  public bool EditPlayer { get; set; }
+  public bool ViewMailbox { get; set; }
+  public bool EditMailbox { get; set; }
+  public bool ViewToken { get; set; }
+  public bool EditToken { get; set; }
+  public bool ViewConfig { get; set; }
+  public bool EditConfig { get; set; }
 
-	public void SetAdmin()
-	{
-		Admin = true;
-		ManagePermissions = true;
-	}
+  public void SetAdmin()
+  {
+    Admin = true;
+    ManagePermissions = true;
+  }
 
-	public void SetUser()
-	{
-		ViewPlayer = true;
-		ViewMailbox = true;
-		ViewToken = true;
-		ViewConfig = true;
-	}
+  public void SetUser()
+  {
+    ViewPlayer = true;
+    ViewMailbox = true;
+    ViewToken = true;
+    ViewConfig = true;
+  }
 }
