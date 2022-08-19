d0b712921358827a5961bd7626238271dc7546ae
Nathan Mac <nathan.mac@rumbleentertainment.com>
2022-08-19 11:33:32 -0700

    old mailbox functionality

--- Models/Permissions/MailPermissions.cs
+++ Models/Permissions/MailPermissions.cs
@@ -4,6 +4,7 @@ public class MailPermissions : PermissionGroup
 {
     public override string Name => "Mail Service";
 
-    public bool SendDirectMessages { get; set; } // TODO: Implement
-    public bool SendGlobalMessages { get; set; } // TODO: Implement
+    public bool Send_Direct_Messages   { get; set; } // TODO: Implement
+    public bool Send_Global_Messages   { get; set; } // TODO: Implement
+    public bool Delete_Global_Messages { get; set; }
 }
