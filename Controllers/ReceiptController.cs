47e2be7c710e25396703b55515ff0e1142704d5e
Nathan Mac <nathan.mac@rumbleentertainment.com>
2022-08-29 10:22:28 -0700

    receipt support

---
+++ Controllers/ReceiptController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Rumble.Platform.Common.Attributes;
+using Rumble.Platform.Common.Utilities;
+
+namespace TowerPortal.Controllers;
+
+[Microsoft.AspNetCore.Components.Route("portal/receipt"), RequireAuth(AuthType.ADMIN_TOKEN)]
+public class ReceiptController : PortalController
+{
+  #region Fetch receipts
+  // Gets all receipts
+  [HttpGet, Route("all")]
+  public ActionResult All()
+  {
+    Require(Permissions.Receipt.View_Page);
+
+    return Forward("/commerce/admin/all");
+  }
+  
+  // Gets receipts for a player
+  [HttpGet, Route("player")]
+  public ActionResult Player(string accountId)
+  {
+    Require(Permissions.Receipt.View_Page);
+
+    return Forward("/commerce/admin/player");
+  }
+  #endregion
+}
