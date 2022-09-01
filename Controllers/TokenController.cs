92b53418de87b710afd67cbcab0a104d111d839e
Will Maynard <will@willmaynard.com>
2022-09-01 10:27:45 -0700

    Add token status endpoint

--- Controllers/TokenController.cs
+++ Controllers/TokenController.cs
@@ -7,34 +7,43 @@ namespace TowerPortal.Controllers;
 [Route("portal/token"), RequireAuth(AuthType.ADMIN_TOKEN)]
 public class TokenController : PortalController
 {
-  #region Player bans
-  // Bans a player
-  [HttpPatch, Route("ban")]
-  public ActionResult Ban()
-  {
-    Require(Permissions.Token.Ban);
+#region Player bans
 
-    return Forward("/token/admin/ban");
-  }
-  
-  // Unbans a player
-  [HttpPatch, Route("unban")]
-  public ActionResult Unban()
-  {
-    Require(Permissions.Token.Unban);
+    [HttpGet, Route("status")]
+    public ActionResult BanStatus()
+    {
+        Require(Permissions.Token.View_Page);
 
-    return Forward("/token/admin/unban");
-  }
-  #endregion
-  
-  #region Token invalidation
-  // Invalidate a player's token
-  [HttpPatch, Route("invalidate")]
-  public ActionResult Invalidate()
-  {
-    Require(Permissions.Token.Invalidate);
+        return Forward("/token/admin/status");
+    }
 
-    return Forward("/token/admin/invalidate");
-  }
-  #endregion
+    // Bans a player
+    [HttpPatch, Route("ban")]
+    public ActionResult Ban()
+    {
+        Require(Permissions.Token.Ban);
+
+        return Forward("/token/admin/ban");
+    }
+
+    // Unbans a player
+    [HttpPatch, Route("unban")]
+    public ActionResult Unban()
+    {
+        Require(Permissions.Token.Unban);
+
+        return Forward("/token/admin/unban");
+    }
+#endregion
+
+#region Token invalidation
+    // Invalidate a player's token
+    [HttpPatch, Route("invalidate")]
+    public ActionResult Invalidate()
+    {
+        Require(Permissions.Token.Invalidate);
+
+        return Forward("/token/admin/invalidate");
+    }
+#endregion
 }
