11ad724ee7847f373f4e844fc782cf04eff663c2
Nathan Mac <nathan.mac@rumbleentertainment.com>
2022-10-28 10:14:42 -0700

    auth token type

--- Controllers/CalendarController.cs
+++ Controllers/CalendarController.cs
@@ -1,9 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Rumble.Platform.Common.Attributes;
+using Rumble.Platform.Common.Utilities;
 
 namespace Dmz.Controllers;
 
-[Route("dmz/calendar"), RequireAuth()]
+[Route("dmz/calendar"), RequireAuth(AuthType.ADMIN_TOKEN)]
 public class CalendarController : DmzController
 {
 	[HttpGet, Route("events")]
