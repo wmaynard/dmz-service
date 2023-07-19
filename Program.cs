9fc99e734287899a6011affd80fa3786c6d219de
Will Maynard <will@willmaynard.com>
2023-07-19 00:57:22 -0700

    Prune old wwwroot

--- Program.cs
+++ Program.cs
@@ -1,15 +1,8 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Dmz
 {
@@ -27,7 +20,6 @@ namespace Dmz
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
