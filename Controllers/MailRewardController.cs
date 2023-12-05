6da05c3743f217692358bfc323f1bb993e0a6158
Will Maynard <will@willmaynard.com>
2023-12-05 14:01:52 -0800

    Updates to mail campaign claims

---
+++ Controllers/MailRewardController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using Dmz.Exceptions;
+using Dmz.Interop;
+using Microsoft.AspNetCore.Mvc;
+using RCL.Logging;
+using Rumble.Platform.Common.Attributes;
+using Rumble.Platform.Common.Enums;
+using Rumble.Platform.Common.Exceptions;
+using Rumble.Platform.Common.Extensions;
+using Rumble.Platform.Common.Minq;
+using Rumble.Platform.Common.Models;
+using Rumble.Platform.Common.Services;
+using Rumble.Platform.Common.Utilities;
+using Rumble.Platform.Common.Web;
+using Rumble.Platform.Data;
+
+namespace Dmz.Controllers;
+
+[Route("/dmz/rewards"), RequireAuth]
+public class MailRewardController : DmzController
+{
+    #pragma warning disable
+    
+    private readonly DynamicConfig _config;
+    #pragma warning restore
+    
+    [HttpGet, Route("claim"), NoAuth]
+    public ActionResult ClaimReward()
+    {
+        string redirectUrl = "https://towersandtitans.com";
+        try
+        {
+            Forward("/mail/claim", out RumbleJson response);
+
+            redirectUrl = response
+                .Optional<RumbleJson>("mailboxMessage")
+                ?.Optional<RumbleJson>("data")
+                ?.Optional<string>("successUrl")
+                ?? _config.Optional<string>("mailClaimSuccessPage")
+                ?? redirectUrl;
+
+            return Redirect(redirectUrl);
+        }
+        catch (ForwardingException e)
+        {
+            Log.Error(Owner.Will, "Unable to forward mail claim request", exception: e);
+            
+            redirectUrl = _config.Optional<string>("mailClaimFailurePage") ?? redirectUrl;
+            return Redirect(redirectUrl);
+        }
+    }
+}
--- Interop/MailchimpSubscriptionStatus.cs
+++
@@ -1,8 +0,0 @@
-namespace Dmz.Interop;
-
-public enum MailchimpSubscriptionStatus
-{
-    Unsubscribed = 0,
-    Subscribed = 1,
-    Unknown = 2
-}
