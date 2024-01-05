119527dfe073a95efbfd3d4e88abd6756a89408b
Will Maynard <will@willmaynard.com>
2024-01-04 17:32:29 -0800

    Add cleanup task to activity logs

--- Services/ActivityLogService.cs
+++ Services/ActivityLogService.cs
@@ -1,11 +1,13 @@
 using Dmz.Models.Portal;
+using RCL.Logging;
 using Rumble.Platform.Common.Minq;
+using Rumble.Platform.Common.Utilities;
 
 namespace Dmz.Services;
 
-public class ActivityLogService : MinqService<AuditLog>
+public class ActivityLogService : MinqTimerService<AuditLog>
 {
-    public ActivityLogService() : base("activities") { }
+    public ActivityLogService() : base("activities", IntervalMs.FourHours) { }
 
     public AuditLog[] Page(int size, int page, string accountId, out long remaining) => mongo
         .Where(query =>
@@ -15,4 +17,12 @@ public class ActivityLogService : MinqService<AuditLog>
         })
         .Sort(sort => sort.OrderByDescending(log => log.CreatedOn))
         .Page(size, page, out remaining);
+
+    protected override void OnElapsed() => mongo
+        .Where(query => query.LessThanOrEqualTo(log => log.CreatedOn, Timestamp.SixMonthsAgo))
+        .OnRecordsAffected(result => Log.Info(Owner.Will, "Old DMZ activity logs deleted.", data: new
+        {
+            Affected = result.Affected
+        }))
+        .Delete();
 }
